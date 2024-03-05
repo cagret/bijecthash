b88bb19e0f098b2b04cb26f02f11dfc7bb9044b9
cagret <cagret@mailo.com>
2024-03-05 17:14:30 +0100

    add k1 in bench

--- run_methods.sh
+++ run_methods.sh
@@ -3,14 +3,20 @@
 # Définir le chemin du fichier fasta et la longueur à utiliser
 fasta_path="resources/SRR072008.fastq"
 length="30"
-
+k1_values=(8 9 10 11 12 13)
 # Liste des méthodes à exécuter
 methods=(identity random cyclic inverse zigzag inthash GAB)
 
 rm result.txt
 
+# Boucle pour chaque méthode
 for method in "${methods[@]}"; do
     echo "Exécution avec la méthode : $method"
-    ./src/BijectHash "$fasta_path" "$length" "$method" >> result.txt
-done
 
+    # Boucle pour chaque valeur de k1
+    for k1 in "${k1_values[@]}"; do
+        echo "k1 = $k1"
+        ./src/BijectHash "$fasta_path" "$length" "$method" "$k1" >> result.txt
+    done
+
+done
