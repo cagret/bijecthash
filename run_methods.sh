e3d9d9037de3bb6b5b99dfa7c056f24941a87960
cagret <cagret@mailo.com>
2024-03-05 18:21:40 +0100

    Add -p option to parallelise the benchmark

--- run_methods.sh
+++ run_methods.sh
@@ -1,22 +1,54 @@
 #!/bin/bash
 
+# Vérification du nombre d'arguments
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 [-p num_processes]"
+    exit 1
+fi
+
 # Définir le chemin du fichier fasta et la longueur à utiliser
-fasta_path="resources/SRR072008.fastq"
-length="30"
 k1_values=(8 9 10 11 12 13)
 # Liste des méthodes à exécuter
 methods=(identity random cyclic inverse zigzag inthash GAB)
 
-rm result.txt
+rm -f result.txt
 
-# Boucle pour chaque méthode
-for method in "${methods[@]}"; do
-    echo "Exécution avec la méthode : $method"
+# Fonction pour exécuter une méthode avec une valeur de k1
+run_method() {
+    fasta_path="resources/SRR072008.fastq"
+    k=30
+    method="$1"
+    k1="$2"
+    temp_file=$(mktemp)
+    echo "k= $k"
+    echo "k1= $k1"
+    ./src/BijectHash "$fasta_path" $k "$method" $k1 >> "$temp_file"
+    cat "$temp_file" >> result.txt
+    rm "$temp_file"
+}
 
-    # Boucle pour chaque valeur de k1
-    for k1 in "${k1_values[@]}"; do
-        echo "k1 = $k1"
-        ./src/BijectHash "$fasta_path" "$length" "$method" "$k1" >> result.txt
+export -f run_method
+
+# Vérifier s'il y a une option pour le nombre de processus
+if [ "$1" == "-p" ]; then
+    if [ "$#" -lt 2 ]; then
+        echo "Usage: $0 -p num_processes"
+        exit 1
+    fi
+    num_processes="$2"
+    for method in "${methods[@]}"; do
+        echo "Exécution avec la méthode : $method"
+        parallel -j "$num_processes" run_method ::: "$method" ::: "${k1_values[@]}"
     done
+else
+    for method in "${methods[@]}"; do
+        echo "Exécution avec la méthode : $method"
+        for k1 in "${k1_values[@]}"; do
+            run_method "$method" "$k1"
+        done
+    done
+fi
 
-done
+# Fermer le fichier result.txt
+exec 3>&-
+
