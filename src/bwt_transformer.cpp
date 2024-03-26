27cea459aa54e5d5d96f04ec0871812b1a138ceb
Clement <cagret@mailo.com>
2024-03-26 13:38:21 +0100

    BWT as Lyndon

--- src/bwt_transformer.cpp
+++ src/bwt_transformer.cpp
@@ -9,29 +9,41 @@ BwtTransformer::BwtTransformer(const Settings& s) : Transformer(s, "Bwt") {
 Transformer::EncodedKmer BwtTransformer::operator()(const std::string& kmer) const {
     EncodedKmer encoded;
     int n = kmer.length();
-    size_t bwt_pos = 0;
+    size_t bwt_pos =0;
+    int pos = n - 1;
     std::vector<std::string> rotations(n);
     
     for (int i = 0; i < n; ++i) {
-        int shift = (i + bwt_pos) % n;
+        int shift = (i + pos) % n;
         rotations[i] = kmer.substr(shift) + kmer.substr(0, shift);
     }
 
     std::sort(rotations.begin(), rotations.end());
-    int newEndPos = 0;
     std::string result;
     for (int i = 0; i < n; ++i) {
         result += rotations[i][n - 1];
-        if (rotations[i].substr(n - bwt_pos - 1) == kmer.substr(0, bwt_pos + 1) &&
-            rotations[i].substr(0, n - bwt_pos - 1) == kmer.substr(bwt_pos + 1)) {
-            newEndPos = i;
+        if (rotations[i].substr(n - pos - 1) == kmer.substr(0, pos + 1) &&
+            rotations[i].substr(0, n - pos - 1) == kmer.substr(pos + 1)) {
+            bwt_pos = i;
         }
     }
-    bwt_pos = newEndPos;
+    
+#ifdef DEBUG
+    std::cerr << "[DEBUG] " << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__ << ":"
+       << " kmer: '" << kmer << "'" 
+       << " BWT rotation index: '" << bwt_pos << "'" << std::endl;
+#endif
+
     assert(bwt_pos < 64); // lyndon_pos can be encoded in only 6 bits
     std::string bwt_rotation;
     bwt_rotation.reserve(kmer.size());
     bwt_rotation = kmer.substr(bwt_pos) + kmer.substr(0, bwt_pos);
+    assert(bwt_rotation.size() == kmer.size());
+
+#ifdef DEBUG
+    std::cerr << "[DEBUG] " << __FILE__ << ":" << __LINE__ << ":" << __FUNCTION__ << ":"
+       << " BWT rotation: '" << bwt_rotation << "'" << std::endl;
+#endif
 
     encoded.prefix = _encode(bwt_rotation.c_str(), settings.prefix_length);
     encoded.suffix = _encode(bwt_rotation.c_str()+settings.prefix_length, settings.length - settings.prefix_length);
@@ -48,28 +60,6 @@ std::string BwtTransformer::operator()(const Transformer::EncodedKmer& e) const 
     std::string suffix = _decode(e.suffix, settings.length - settings.prefix_length);
     std::string bwt_rotation = prefix + suffix;
     std::string bwt = bwt_rotation.substr(bwt_rotation.size() - bwt_pos) + bwt_rotation.substr(0, bwt_rotation.size() - bwt_pos);
-
-    int n = bwt.length();
-    std::vector<int> T(n);
-    std::map<char, std::vector<int>> charPositions;
-
-    for (int i = 0; i < n; ++i) {
-        charPositions[bwt[i]].push_back(i);
-    }
-
-    int index = 0;
-    for (auto& p : charPositions) {
-        for (int pos : p.second) {
-            T[index++] = pos;
-        }
-    }
-
-    std::string original(n, ' ');
-    int next = T[bwt_pos]; 
-    for (int i = n - 1; i >= 0; --i) {
-        original[i] = bwt[next];
-        next = T[next];
-    }
-
-    return original;
+    
+    return bwt;
 }
